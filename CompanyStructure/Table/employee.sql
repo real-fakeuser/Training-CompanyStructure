d4dc5585519c9f71442e79395f9765cc8b63525c
realfakeuser <tim.niewerth@tobit.software>
2018-09-05 16:38:21 +0200

    added empleyee table and attributes

---
+++ CompanyStructure/Table/employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[employee]
+(
+	[employee_Id] INT NOT NULL PRIMARY KEY,
+	[employee_Name] nvarchar(256) not null,
+	[employee_BirthDate] Datetime2(7) not null,
+	[CreationTime] Datetime2(7) not null default GetDate(),
+	[DeleteTime] Datetime2(7)
+)
