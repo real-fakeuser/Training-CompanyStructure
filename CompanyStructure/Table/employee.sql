ff9d48cae3942a7439b65088289a8e88bc02e1f3
realfakeuser <tim.niewerth@tobit.software>
2018-09-05 17:37:56 +0200

    employee table added

--- CompanyStructure/Table/employee.sql
+++ CompanyStructure/Table/employee.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[employee]
 (
-	[employee_Id] INT NOT NULL PRIMARY KEY,
-	[employee_Name] nvarchar(256) not null,
-	[employee_BirthDate] Datetime2(7) not null,
+	[Employee_Id] INT NOT NULL PRIMARY KEY,
+	[Branch_Id] int not null REFERENCES Branches[Branch_Id],
+	[Employee_Name] nvarchar(256) not null,
+	[Employee_BirthDate] Datetime2(7) not null,
 	[CreationTime] Datetime2(7) not null default GetDate(),
 	[DeleteTime] Datetime2(7)
 )
