2924c7ca3766588e2d1885c9c24300e09cf33ad8
realfakeuser <tim.niewerth@tobit.software>
2018-09-06 09:08:31 +0200

    edited Id attribute for all tables

--- CompanyStructure/Table/Branch.sql
+++ CompanyStructure/Table/Branch.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Branch]
 (
-	[Branch_Id] INT NOT NULL PRIMARY KEY,
-	[Manager_Id] int not null REFERENCES Employee(Employee_Id),
-	[Company_Id] int not null REFERENCES Company(Company_Id),
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Manager_Id] int not null REFERENCES Employee(Id),
+	[Company_Id] int not null REFERENCES Company(Id),
 	[Employee_BirthDate] Datetime2(7) not null,
 	[CreationTime] Datetime2(7) not null default GetDate(),
 	[DeleteTime] Datetime2(7))
