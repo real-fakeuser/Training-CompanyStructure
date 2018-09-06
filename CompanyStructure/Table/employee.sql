f05c6871b72edb50cc84b44c61b9f33d0a47c257
realfakeuser <tim.niewerth@tobit.software>
2018-09-06 13:55:20 +0200

    project deleted

--- CompanyStructure/Table/employee.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[employee]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Branch_Id] int not null REFERENCES Branch(Id),
-	[Employee_Name] nvarchar(256) not null,
-	[Employee_BirthDate] Datetime2(7) not null,
-	[CreationTime] Datetime2(7) not null default GetDate(),
-	[DeleteTime] Datetime2(7)
-)
--- CompanyStructure/View/viCompany.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE VIEW [dbo].[viCompany]
-	AS	SELECT C.Id, C.Company_Name
-		FROM [Company] as C
